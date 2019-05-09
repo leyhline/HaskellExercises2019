76806f7f130150f186036a229147dafb574703be
Thomas Leyh <thomas.leyh@mailbox.org>
2019-05-09 19:35:18 +0200

    Finished exercise 1 of sheet 2

---
+++ src/Sheet2/Ex1.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Sheet2.Ex1 where
+
+import Test.QuickCheck.All
+
+fib :: Int -> Integer
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
+
+fibFast :: Int -> Integer
+fibFast n = fibList !! n 
+
+fibList :: [Integer]
+fibList = 0 : 1 : zipWith (+) fibList (tail fibList)
+
+prop_CompareWithNaiveFib n = fib (mod n 20) == fibFast (mod n 20)
+
+return []
+runTests :: IO Bool
+runTests = $quickCheckAll
