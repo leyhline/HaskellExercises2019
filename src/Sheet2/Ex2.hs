0e1e87463ed7c03c768aa0e18b6c84a8dcb4d325
Thomas Leyh <thomas.leyh@mailbox.org>
2019-05-10 09:33:19 +0200

    Finished ex2-sheet 2 (todo: higher order funcs)

---
+++ src/Sheet2/Ex2.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Sheet2.Ex2 where
+
+import Data.List
+import Test.QuickCheck.All
+
+undup :: Eq a => [a] -> [a]
+undup x = reverse $ buildUndupList ([], x)
+
+buildUndupList :: Eq a => ([a], [a]) -> [a]
+buildUndupList (unduplicated, []) = unduplicated
+buildUndupList (unduplicated, current:remaining)
+    = if elem current unduplicated
+        then buildUndupList (unduplicated, remaining)
+        else buildUndupList (current:unduplicated, remaining)
+
+prop_CompareWithNub x = undup x == nub x
+
+return []
+runTests :: IO Bool
+runTests = $quickCheckAll
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,6 +3,7 @@ import Sheet1.Ex1 as Sheet1Ex1
 import Sheet1.Ex2 as Sheet1Ex2
 import Sheet1.Ex3 as Sheet1Ex3
 import Sheet2.Ex1 as Sheet2Ex1
+import Sheet2.Ex2 as Sheet2Ex2
 
 main :: IO ()
 main = do
@@ -10,7 +11,8 @@ main = do
         Sheet1Ex1.runTests,
         Sheet1Ex2.runTests,
         Sheet1Ex3.runTests,
-        Sheet2Ex1.runTests]
+        Sheet2Ex1.runTests,
+        Sheet2Ex2.runTests]
     if good
         then exitSuccess
         else exitFailure
