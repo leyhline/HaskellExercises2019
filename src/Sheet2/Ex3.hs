b514d14ab1ca25c4d2f8f3df5019387d62b8c1dc
Thomas Leyh <thomas.leyh@mailbox.org>
2019-05-19 23:33:42 +0200

    Finished ex3 on sheet 2

--- src/Sheet2/Ex3.hs
+++ src/Sheet2/Ex3.hs
@@ -12,6 +12,15 @@ smallestFactor n
             ms = [1..(div n 2)]
             products = zipWith (\k ms -> map (\m -> k * m) ms) ks (repeat ms)
 
+smallestFactor' :: Int -> Int
+smallestFactor' n
+    | n < 2 = error "There is no factor for numbers smaller 2."
+    | otherwise = minimum ks
+        where ks = filter (>= 2) $ map fst $ filter (\x -> snd x == 0) $ map (\m -> (div n m, mod n m)) [1..n]
+
+prop_CompareSmallestFactorImplementations n = smallestFactor n' == smallestFactor' n'
+    where n' = abs n + 2
+
 return []
 runTests :: IO Bool
 runTests = $quickCheckAll
