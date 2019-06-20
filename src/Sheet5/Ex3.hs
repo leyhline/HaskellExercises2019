de95955017ee6211655dab701d5bf12acc62505e
Thomas Leyh <thomas.leyh@mailbox.org>
2019-06-20 21:57:00 +0200

    Using ParserCon from lecture and writing down some solutions

--- src/Sheet5/Ex3.hs
+++ src/Sheet5/Ex3.hs
@@ -1,80 +1,24 @@
 module Sheet5.Ex3 where
 
-import Control.Applicative
-import Control.Monad
-
--- Definitions from lecture:
-
-data Term  = Con Integer | Bin Term Op Term  
-    deriving (Eq, Show)
-           
-data Op = Add | Sub | Mul | Div
-    deriving (Eq, Show)
-
-newtype Parser token result = Parser {runParser :: [token] -> [(result, [token])]}
-    
-succeed :: r -> Parser t r
-succeed r = Parser $ \ ts -> [(r, ts)]
-
-satisfy :: (t -> Bool) -> Parser t t
-satisfy p = Parser g
-    where
-        g (t:ts) | p t = [(t, ts)]
-        g _ = []
-
-lit :: Eq t => t -> Parser t t
-lit t = satisfy (== t)
-
-pseq :: Parser t (s -> r) -> Parser t s -> Parser t r
-pseq p1 p2 = Parser $ \ts ->
-    let srs = runParser p1 ts -- :: [(s -> r, [t])]
-        g (srf, ts1) = map (h srf) $ runParser p2 ts1 -- [(s, [t])]
-        h srf (s, ts2) = (srf s, ts2)
-        rs  = map g srs -- [[(r,[t])]]
-    in concat rs
-
-pempty :: Parser t r
-pempty = Parser $ const []
-
-palt :: Parser t r -> Parser t r -> Parser t r
-palt p1 p2 =
-    Parser $ \ts -> runParser p1 ts ++ runParser p2 ts
-
-pmap :: (s -> r) -> Parser t s -> Parser t r
-pmap f p = Parser (map g . runParser p)
-    where g (s, ts) = (f s, ts)
-
-instance Functor (Parser token) where
-    fmap = pmap
-
-instance Applicative (Parser token) where
-    pure = succeed
-    (<*>) = pseq
-
-instance Alternative (Parser token) where
-    empty = pempty
-    (<|>) = palt
-
-instance Monad (Parser token) where
-    return = succeed
-    (Parser p) >>= f =
-        Parser (concatMap g . p)
-        where g (a, ts) = runParser (f a) ts
-
-instance MonadPlus (Parser t) where
-    mzero = pempty
-    mplus = palt
-
--- Actual exercise:
+import Data.Char
+import ParserCon
 
 pmany :: Parser t r -> Parser t [r]
-pmany = undefined
+pmany p = ((:) <$> p <*> pmany p) <|> pure []
 
 pmany1 :: Parser t r -> Parser t [r]
-pmany1 = undefined
+pmany1 p = (:) <$> p <*> pmany p
+
+pIntersperse :: Parser t r -> Parser t w -> Parser t [r]
+pIntersperse pThing pSep = undefined
+
+pInt :: Parser Char Integer
+pInt = read <$> pmany1 (satisfy isDigit)
 
 pIntList :: Parser Char [Integer]
-pIntList = undefined
+pIntList = lit '[' *> pmany p <* lit ']'
+    where
+        p = pInt <* lit ','
 
 pPaliAB :: Parser Char String
 pPaliAB = undefined
