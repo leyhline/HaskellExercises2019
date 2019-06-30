cd75097ada1b3697ca803d3377e15c73677c5e63
Thomas Leyh <thomas.leyh@mailbox.org>
2019-06-30 12:41:43 +0200

    Logging with timestamp

--- src/Sheet7/Ex2.hs
+++ src/Sheet7/Ex2.hs
@@ -1,6 +1,7 @@
 module Sheet7.Ex2 where
 
-import Control.Monad.Writer.Strict
+import Control.Monad.Trans.Writer.Strict
+import Data.Time.Clock.POSIX
 
 data Item = Msg String
           | Section String [Item]
@@ -8,16 +9,23 @@ data Item = Msg String
 
 type Log = [Item]
 
-type Logging a = Writer Log a
+type Logging a = WriterT Log IO a
 
 log :: Show t => t -> Logging ()
-log s = let message = Msg $ show s in
-    writer ((), [message])
+log s = WriterT $ do
+    time <- getPOSIXTime
+    let message = Msg $ show time ++ ": " ++ show s
+    return ((), [message])
 
 withSection :: String -> Logging a -> Logging a
-withSection sectionName logging =
-    let (a, log) = runWriter logging in
-        writer (a, [Section sectionName log])
+withSection sectionName logging = WriterT $ do
+    timeBefore <- getPOSIXTime
+    (a, log) <- runLogging logging
+    timeAfter <- getPOSIXTime
+    return (a,
+        [Msg $ show timeBefore ++ ": " ++ "SECTION START"
+        ,Section sectionName log
+        ,Msg $show timeAfter ++ ": " ++ "SECTION END"])
 
-runLogging :: Logging a -> (a, Log)
-runLogging = runWriter
+runLogging :: Logging a -> IO (a, Log)
+runLogging = runWriterT
